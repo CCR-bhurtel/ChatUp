8d91fdffc5c3c6a2bb909ddffe68a2eab53aceab
cceer <sisir.sital@gmail.com>
2023-12-16 13:47:24 +0545

    update: chat flow

--- api/src/controllers/chat/groupChat.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'express';
-import { ExpressRequest } from '../../Types/User';
-import catchAsync from '../../utils/catchAsync';
-
-export const createGroupChat = catchAsync(async (req: ExpressRequest, res: Response, next) => {});
-
-export const updateGroupChat = catchAsync(async (req: ExpressRequest, res: Response, next) => {});
--- api/src/controllers/chat/index.ts
+++ api/src/controllers/chat/index.ts
@@ -1,12 +1,6 @@
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { ExpressRequest } from '../../Types/User';
 import catchAsync from '../../utils/catchAsync';
 
-export const searchUsers = catchAsync(async (req: ExpressRequest, res: Response, next) => {});
-
-export const createRoom = catchAsync(async (req: ExpressRequest, res: Response, next) => {});
-
 export const sendMessage = catchAsync(async (req: ExpressRequest, res: Response, next) => {});
-
-
-
+export const deleteMessage = catchAsync(async (req: ExpressRequest, res: Response, next: NextFunction) => {});
