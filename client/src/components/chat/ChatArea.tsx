dc9f391ac6dd9229744632743abd30bab0a75f2c
Tamrakar182 <manjultamrakar4@gmail.com>
2023-12-17 21:45:51 +0545

    add closing of emoji select and moved to seperate component

--- client/src/components/chat/ChatArea.tsx
+++ client/src/components/chat/ChatArea.tsx
@@ -1,15 +1,11 @@
 import {
-    faFaceSmile,
-    faPaperPlane,
-    faPaperclip,
     faPhone,
     faTrashCan,
     faVideoCamera,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { ChangeEvent, SyntheticEvent, createRef, useEffect, useMemo, useState } from 'react';
+import React, { SyntheticEvent, createRef, useEffect, useMemo, useState, useRef } from 'react';
 import Avatar from '../reusables/Avatar';
-import { userData } from '@/data/testUser';
 import { IActiveRoom } from '@/Types/Room';
 import getAvatarImage from '@/utils/getAvatarImage';
 import MessageContainer from './MessageContainer';
@@ -20,22 +16,20 @@ import { useRoom } from '@/context/chat/RoomContextProvider';
 import { RoomActionTypes } from '@/context/chat/roomActions';
 import toast from 'react-hot-toast';
 import { getSocket } from '@/utils/socketService';
-import Picker from '@emoji-mart/react';
-import data, { Skin } from '@emoji-mart/data';
+import MessageInput from './MessageInput';
 
 interface IChatArea {
     handleInfoOpen?: () => void;
     room: IActiveRoom;
 }
+
 function ChatArea(props: IChatArea) {
     const { room } = props;
     const auth = useAuth();
     const { state, dispatch } = useRoom();
-    const [message, setMessage] = useState<string>('');
     const divref = createRef<HTMLDivElement>();
 
     const [typingUser, setTypingUser] = useState<string | null>(null);
-    const [showPicker, setShowPicker] = useState(false);
 
     const socket = useMemo(() => getSocket(), []);
     useEffect(() => {
@@ -48,11 +42,7 @@ function ChatArea(props: IChatArea) {
     }, [state.activeRoom?.messages, divref]);
     const roomImage = useMemo(() => getAvatarImage(room.roomImage, room.isGroupChat), [room]);
 
-    const handleMessageChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setMessage(e.target.value);
-    };
-
-    const handleSendMessage = async (e: SyntheticEvent) => {
+    const handleSendMessage = async (e: SyntheticEvent, message: string) => {
         e.preventDefault();
         if (!auth.state.user) {
             toast.error('User not authorized');
@@ -71,17 +61,10 @@ function ChatArea(props: IChatArea) {
             };
 
             dispatch({ type: RoomActionTypes.AppendChatToRoom, payload: chat });
-            setMessage('');
-
             socket.emit('newMessage', { ...chat, room: state.activeRoom });
         } catch (err: any) {
             toast.error(err.response?.data.message);
         }
-    };
-
-    const onEmojiClick = (obj: Skin) => {
-        setMessage(message + obj.native);
-        setShowPicker(false);
     };
 
     useEffect(() => {
@@ -91,6 +74,8 @@ function ChatArea(props: IChatArea) {
             dispatch({ type: RoomActionTypes.AppendChatToRoom, payload: message });
         });
     }, []);
+
+    
 
     return (
         <div className="flex chatsection relative  flex-col h-full w-full">
@@ -119,39 +104,7 @@ function ChatArea(props: IChatArea) {
                         <MessageContainer messages={room.messages} />
                     </div>
                 </div>
-                <div className="messageBox absolute w-full justify-center self-end bottom-2 flex p-4">
-                    <div className="bg-cgray p-4 rounded-tl-xl text-sm rounded-bl-xl flex flex-1 items-center">
-                        <form onSubmit={handleSendMessage} className="flex-1">
-                            <input
-                                value={message}
-                                onChange={handleMessageChange}
-                                type="text"
-                                className="w-full outline-none placeholder:font-thin text-white font-light text-sm placeholder:text-white bg-transparent focus:bg-transparent border-none outline-0 focus:outline-0"
-                                placeholder="Your message here "
-                            />
-                        </form>
-
-                        <div className="flex md:relative  gap-2 self-end justify-end">
-                            <FontAwesomeIcon icon={faPaperclip} style={{ color: 'white' }} />
-
-                            <div className="emojicontainer ">
-                                <FontAwesomeIcon
-                                    icon={faFaceSmile}
-                                    style={{ color: 'white', cursor: 'pointer' }}
-                                    onClick={() => setShowPicker(true)}
-                                />
-                                {showPicker && (
-                                    <div className="absolute left-0 bottom-10 md:left-auto md:right-0">
-                                        <Picker data={data} onEmojiSelect={onEmojiClick} />
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="sendButton bg-navy hover:bg-secondary px-4 items-center justify-center flex rounded-tr-md rounded-br-md">
-                        <FontAwesomeIcon icon={faPaperPlane} style={{ color: 'white' }} />
-                    </div>
-                </div>
+                <MessageInput handleSendMessage={handleSendMessage} />
             </div>
         </div>
     );
