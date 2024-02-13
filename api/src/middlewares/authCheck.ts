2ff1f88f86ec7049bfdd1d49a5772249b1f6da63
CCR-bhurtel <bhurtelshishir@gmail.com>
2024-02-13 15:57:56 +0545

    fix: swagger definition

--- api/src/middlewares/authCheck.ts
+++ api/src/middlewares/authCheck.ts
@@ -1,27 +1,26 @@
-import { NextFunction, Response } from 'express';
-import parseToken from '../utils/parseToken';
-import jwt from 'jsonwebtoken';
-import User from '../database/Model/User';
-import { JWT_SECRET } from '../config/keys';
-import { PopulatedUser } from '../Types/User';
+import { NextFunction, Response } from "express";
+import parseToken from "../utils/parseToken";
+import jwt from "jsonwebtoken";
+import User from "../database/Model/User";
+import { JWT_SECRET } from "../config/keys";
 
 const authCheck = async (req: any, res: Response, next: NextFunction) => {
-    const authToken: string | null = parseToken(req);
+  const authToken: string | null = parseToken(req);
 
-    if (!authToken) {
-        return res.status(403).json({ message: 'Error authenticating' });
-    }
-    if (authToken) {
-        try {
-            const decodedPayload: any = jwt.verify(authToken, JWT_SECRET);
-            const user = await User.findById(decodedPayload.id).select(['-password']);
+  if (!authToken) {
+    return res.status(403).json({ message: "Error authenticating" });
+  }
+  if (authToken) {
+    try {
+      const decodedPayload: any = jwt.verify(authToken, JWT_SECRET);
+      const user = await User.findById(decodedPayload.id).select(["-password"]);
 
-            req.user = user;
-            next();
-        } catch (err) {
-            next(err);
-        }
+      req.user = user;
+      next();
+    } catch (err) {
+      next(err);
     }
+  }
 };
 
 export default authCheck;
