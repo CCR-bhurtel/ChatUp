55a2ab9737b04d7113257179201a343ab71eabf4
CCR-bhurtel <bhurtelshishir@gmail.com>
2024-04-03 16:15:25 +0545

    fix: using file-loader for static files

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+  reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    // Disable webpack's asset processing for images and fonts
+    if (!isServer) {
+      config.module.rules.forEach((rule) => {
+        if (
+          rule.test &&
+          (rule.test.toString().includes("png") ||
+            rule.test.toString().includes("jpg") ||
+            rule.test.toString().includes("svg") ||
+            rule.test.toString().includes("gif") ||
+            rule.test.toString().includes("woff") ||
+            rule.test.toString().includes("woff2") ||
+            rule.test.toString().includes("ttf") ||
+            rule.test.toString().includes("eot"))
+        ) {
+          rule.use = [
+            {
+              loader: "file-loader",
+              options: {
+                publicPath: "/_next",
+                name: "static/media/[name].[hash].[ext]",
+              },
+            },
+          ];
+        }
+      });
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
