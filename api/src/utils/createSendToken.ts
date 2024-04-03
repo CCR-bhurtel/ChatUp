81933711b215f16695b8bc30efbe4ff2fd9d1b51
CCR-bhurtel <bhurtelshishir@gmail.com>
2024-04-03 17:57:28 +0545

    fix: cookie policy

--- api/src/utils/createSendToken.ts
+++ api/src/utils/createSendToken.ts
@@ -1,25 +1,27 @@
-import { Response } from 'express';
-import jwt from 'jsonwebtoken';
-import { PopulatedUser } from '../Types/User';
-import { JWT_COOKIE_EXPIRES_IN, JWT_SECRET } from '../config/keys';
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+import { PopulatedUser } from "../Types/User";
+import { JWT_COOKIE_EXPIRES_IN, JWT_SECRET } from "../config/keys";
 
 const createSendToken = (user: PopulatedUser, res: Response) => {
-    try {
-        const token: string = jwt.sign({ id: user._id }, JWT_SECRET, {
-            expiresIn: JWT_COOKIE_EXPIRES_IN,
-        });
-        const time: number = parseInt(JWT_COOKIE_EXPIRES_IN);
+  try {
+    const token: string = jwt.sign({ id: user._id }, JWT_SECRET, {
+      expiresIn: JWT_COOKIE_EXPIRES_IN,
+    });
+    const time: number = parseInt(JWT_COOKIE_EXPIRES_IN);
 
-        res.cookie('Authorization', token, {
-            expires: new Date(Date.now() + time * 24 * 60 * 60 * 1000),
+    res.cookie("Authorization", token, {
+      expires: new Date(Date.now() + time * 24 * 60 * 60 * 1000),
+      sameSite: "none",
+      secure: true,
 
-            httpOnly: process.env.NODE_ENV === 'production',
-        });
-        return res.status(200).json({ ...user._doc, password: '', token });
-    } catch (err) {
-        console.log(err);
-        return res.status(400).json(err);
-    }
+      //   httpOnly: process.env.NODE_ENV === "production",
+    });
+    return res.status(200).json({ ...user._doc, password: "", token });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
 };
 
 export default createSendToken;
